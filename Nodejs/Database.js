66af7bd4b7726266776a1fb801fbabfe3bb4a872
StarkTheGnr <lordsomy@hotmail.com>
2019-04-07 21:09:27 +0200

    Update Database.js

--- Nodejs/Database.js
+++ Nodejs/Database.js
@@ -22,10 +22,33 @@ class Database
 
 	GetPost(PID)
 	{
+		//TODO check privacy
 		let query = `SELECT * FROM Post WHERE posterid=${PID};`;
 		//TODO Use query
 
 		//return post
+	}
+
+	VotePost(PID, UID, value)
+	{
+		//TODO check privacy
+		let query = `SELECT * FROM Post WHERE posterid=${PID};`;
+		//TODO Use query
+
+		//return post
+	}
+
+	CreateComment(PID, UID, body)
+	{
+		let query = `INSERT INTO Comments VALUES(${PID}, ${UID}, '${body}');`;
+		//TODO Use query
+	}
+
+	Get20Comments(PID, UID, Index)
+	{
+		//TODO check privacy
+		let query = `SELECT * FROM Comments WHERE postid = ${PID};`;
+		//TODO Use query
 	}
 
 	AddFollower(followerid, followedid)
@@ -37,6 +60,18 @@ class Database
 	RemoveFollower(followerid, followedid)
 	{
 		let query = `DELETE FROM Followers WHERE followerid = ${followerid} and followedid = ${followedid};`;
+		//TODO Use query
+	}
+
+	GetFollowers(UID)
+	{
+		let query = `SELECT followerid FROM Followers WHERE followedid = ${UID};`;
+		//TODO Use query
+	}
+
+	GetFollowed(UID)
+	{
+		let query = `SELECT followedid FROM Followers WHERE followerid = ${UID};`;
 		//TODO Use query
 	}
 }
